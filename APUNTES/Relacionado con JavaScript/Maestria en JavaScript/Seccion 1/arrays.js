5c9409fbff8f280061d156c7c047350aaaf153d0
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-04-26 00:02:07 -0600

    Eliminar apuntes

--- APUNTES/Relacionado con JavaScript/Maestria en JavaScript/Seccion 1/arrays.js
+++
@@ -1,8 +0,0 @@
-//arreglo eb javaScript
-var autos = ['bmw', 'audi', 'volvo'];
-console.log(autos); // bmw
-console.log(typeof autos); // object
-
-var z = '';
-console.log(z); // ''
-console.log(typeof z); // string
